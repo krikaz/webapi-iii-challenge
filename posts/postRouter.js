28acb9520fd66db59d83fa19b56108696ff6415d
krikaz <pascal.bou@protonmail.com>
2019-07-10 13:22:58 +0200

    init

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,6 +1,24 @@
-const express = 'express';
+const express = require('express');
+const Posts = require('./postDb');
 
 const router = express.Router();
+
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+  if (Number.isInteger(Number.id)) {
+    req.validate = true;
+    const post = await Posts.getById(id)
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(404).json({ message: 'that id has gone to Mars!' });
+    }
+  } else {
+    res.set('X-Nasty', 'Nasty ID');
+    res.json({ message: "that does not look like an id!!" });
+  }
+};
 
 router.get('/', (req, res) => {
 
@@ -20,8 +38,6 @@ router.put('/:id', (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
 
-};
 
 module.exports = router;
