8364699d83cc756c6da8580aa1ab65b67809e748
krikaz <pascal.bou@protonmail.com>
2019-07-11 14:03:25 +0200

    added dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(3000, () => {
-	console.log('\n* Server Running on http://localhost:3000 *\n');
+const port = process.env.port;
+
+server.listen(port, () => {
+	console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
