df2d410f6f91467275206f9d3e903f26b1eb7525
krikaz <pascal.bou@protonmail.com>
2019-07-11 15:42:47 +0200

    corrected deployment init

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./server.js');
 
-const port = process.env.port;
+const port = process.env.PORT;
 
 server.listen(port, () => {
 	console.log(`\n* Server Running on http://localhost:${port} *\n`);
